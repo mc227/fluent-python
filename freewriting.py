d92b24c119b734fb956d023ddfe8d7877f811f5d
mc227 <markcostales.mark@gmail.com>
2024-01-17 04:16:33 -0500

    progress is awesome

--- freewriting.py
+++ freewriting.py
@@ -1,3 +1,21 @@
 """
 frenchdeck from scratch
 """
+import collections
+
+Card = collections.namedtuple('Card',['rank','suit'])
+
+class Frenchdeck:
+    """
+    docstring
+    """
+    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+    suits = 'spades hearts clubs diamonds'.split()
+
+    def __init__(self):
+        """
+        __init__
+        """
+        
+if __name__ == "__main__":
+    print(Frenchdeck().suits)
